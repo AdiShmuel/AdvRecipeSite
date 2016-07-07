f6e3f1aac552bbf01b04feeddccc5d4e221c3d79
Gal <galcohen92@gmail.com>
2016-07-07 21:45:11 +0300

    appUser - server writing + check from client

---
+++ server/routes/appUserApi.js
@@ -0,0 +1,84 @@
+var appUserDB = require('../accessDb/appUserADB')
+  , recipeDB = require('../accessDb/recipeADB')
+  , ingredientDB = require('../accessDb/ingredientADB')
+
+
+///////////////// appUser
+exports.getAppUser = function (req, res) {
+  console.log('*** GetAppUser API');
+  appUserDB.getAppUser(req.params.email, function(err, appUser) {
+    if (err) {
+      console.log('*** GetAppUser API Err');
+      res.json({
+        appUser: appUser
+      });
+    } else {
+      console.log('*** GetAppUser API Success');
+
+      res.json(appUser);
+    }
+  });
+};
+
+exports.getAllAppUsers = function (req, res) {
+  console.log('*** GetAppUsers API');
+  appUserDB.getAppUsers(function(err, appUsers) {
+    if (err) {
+      console.log('*** GetAppUsers API Err');
+      res.json({
+        appUsers: appUsers
+      });
+    } else {
+      console.log('*** GetAppUsers API OK');
+
+      res.json(appUsers);
+    }
+  });
+};
+
+exports.createAppUser = function (req, res) {
+  console.log('*** AddAppUser API');
+      appUserDB.createAppUser(req.body, function(err){
+        if (err) {
+          console.log('*** AddAppUser API Err');
+          res.json(false);
+        } else {
+          console.log('*** AddAppUser API OK');
+
+          res.json(req.body);
+        }
+      });
+};
+
+exports.editAppUser = function (req, res) {
+  console.log('*** EditAppUser');
+      appUserDB.editAppUser(req.body, function(err) {
+        if (err) {
+          // console.log('*** editAppUser err' + util.inspect(err));
+          console.log('*** EditAppUser API Err');
+          res.json({'status': false});
+        } else {
+          console.log('*** EditAppUser API OK');
+
+          res.json({'status': true});
+        }
+      });
+};
+
+exports.deleteAppUser = function (req, res) {
+  console.log('*** DeleteAppUser API');
+  var email = req.params.email;
+  appUserDB.deleteAppUser(email, function(err) {
+    if (err) {
+      console.log('*** DeleteAppUser API Err');
+      res.json({'status': false});
+    } else {
+      recipeDB.deleteRecipesByAppUser(email, function(err){
+        ingredientDB.deleteIngredientsByAppUser(email, function(err){
+          console.log('*** DeleteAppUser API OK');
+          res.json({'status': true});
+        });
+      });
+    }
+  });
+};
